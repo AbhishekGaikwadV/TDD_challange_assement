069e04f8619b2b5ddc316c34647c4a062e8bb137
Abhishek <abhishek.v..gaikwad@gmail.com>
2024-12-06 15:21:46 +0530

    Remove non-numeric characters from input parameter

--- scripts.js
+++ scripts.js
@@ -1,7 +1,14 @@
 function add(inputNumbers){
-if( inputNumbers === String || inputNumbers === ''){
-   return ''; 
+//Input Validation
+if( typeof inputNumbers === 'string' ||  inputNumbers === ''){
+   
+//   let onlyNumbers = Number(inputNumbers.replace(/[^0-9]/g, ''));
+ let numbers = Number(inputNumbers.replace(/[^0-9]/g, '' ));
+                   
+  return numbers; 
+
 }
+
 }
 
 
--- scripts.test.js
+++ scripts.test.js
@@ -4,12 +4,17 @@ dbMock = [
 '',
 '1',
 '1,2',
-'1,2,3,4,5,'
+'1,2,3,4,5',
+'1,a,z#,4\n',
 ];
 
 describe('Initalizing addFunction', () =>{
-   it('return empty string for empty string', () =>{
-     expect(add('',dbMock)).toBe('');
+//    it(' Input validation, return empty string for empty input string', () =>{
+//      expect(add('',dbMock)).toBe('');
+//    });
+
+   it('Remove non-numeric characters from input parameter', () => {
+    expect(add(dbMock[4],dbMock)).toBe(14);
    });
     // expect(add('1',dbMock)).toBe();
     
