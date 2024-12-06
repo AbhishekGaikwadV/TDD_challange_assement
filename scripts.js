715ea237484eb779690061ec48797cb7351ebe91
Abhishek <abhishek.v..gaikwad@gmail.com>
2024-12-06 21:50:55 +0530

    Final commit for TTD assement

--- scripts.js
+++ scripts.js
@@ -1,31 +1,26 @@
-
- let input = '1,a,z#,4\n';
-function add(inputNumbers){
+ function add(inputNumbers){
 //Input Validation
 if( typeof inputNumbers === 'string' ||  inputNumbers === ''){
    
     // //Remove negative numbers
-    // let negativeNumbers= (inputNumbers.match(/-\d+/g) || []).map(num => parseInt(num, 10));
+    let negativeNumbers= (inputNumbers.match(/-\d+/g) || []).map(num => parseInt(num, 10));
 
     // //Throw exception with the negative numbers found
-    // if(negativeNumbers.length > 0){
-    //     throw new Error(`negative numbers not allowed ${negativeNumbers.join(',')}`);
-    // }         
+    if(negativeNumbers.length > 0){
+        throw new Error(`negative numbers not allowed ${negativeNumbers.join(',')}`);
+    }         
 
     // Remove special characters
     let numbers = inputNumbers.replace(/[^0-9,]/g, '' );
 
    //Parse the string to integers and filter array for non-numerics
     let finalNumbers = numbers.split(',')
-     .map(num => parseInt(num, 10))
-     .filter(num => !isNaN(num));
+    .map(num => parseInt(num, 10))
+    .filter(num => !isNaN(num));
 
-
-   return finalNumbers;
+    // Sum all the values in the array and return the sum as an integer
+    return finalNumbers.reduce((num,sum) => num + sum, 0);  
  }
-
 }
-
-console.log(add(input));
 
 module.exports = add;
